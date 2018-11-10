641e9cd882f378f9c8ba247810f93e6b9d66b6e5
frozen4code <bhpndrsingh306@gmail.com>
2018-11-11 02:46:05 +0530

    uhh

--- config/prod.keys.js
+++ config/prod.keys.js
@@ -28,5 +28,5 @@ let keys = {
 }
 
 // mongodb://monguser:mongpass@192.168.2.2:27017/ps?authSource=admin
-keys.db.uri = `mongodb://${keys.db.options.user}:${keys.db.options.pass}@${keys.db.host}:${keys.db.port}/${keys.db.name}?authSource=${keys.db.options.authSource}`
+keys.db.uri = `mongodb://${keys.db.host}:${keys.db.port}/${keys.db.name}?authSource=${keys.db.options.authSource}`
 module.exports = keys
