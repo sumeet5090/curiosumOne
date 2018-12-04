58a8022e5d3566d223a1c500e12631c0c52f52b1
frozen4code <bhpndrsingh306@gmail.com>
2018-12-04 18:19:34 +0530

    Footer update

--- layouts/AppFooter.vue
+++ layouts/AppFooter.vue
@@ -1,18 +1,21 @@
 <template>
 <div class="container">
-  <div class="row row-grid align-items-center pt-4 mx-3">
-    <div class="col-md-6">
-      <div class="text-primary font-weight-light mb-2 h5"><a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=business@mobilityeng.in&su=Feedback%20Mobility Eng Portal&tf=1" target="_blank" rel="noreferrer" class="hover-me-i-shake">Leave us some feedback!</a></div>
-      <div class="text-dark font-weight-light text-black-50">We would love to hear from you!</div>
+  <div class="row-justify-content-center mb-1">
+    <div class="text-center text-disabled font-weight-light"><small>Copyright © 2018 <a href="http://mobilityeng.in" target="_blank"  rel="noreferrer">Mobility Engineering Consortium Pvt. Ltd.</a> All Rights Reserved.</small></div>
+  </div>
+  <div class="row row-grid align-items-center mx-1">
+    <div class="col-md-6 px-0">
+      <div class="text-center text-md-left mb-2">
+        <a href="mailto:business@mobilityeng.in?subject=MEC Portal | Feedback" target="_blank" rel="noreferrer" class="text-primary font-weight-bold h5">Leave us some feedback!</a>
+      </div>
+      <div class="text-center text-md-left font-weight-light text-disabled"><small>We would love to hear from you!</small></div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-6 px-0 mt-0">
       <div class="d-flex flex-column align-items-sm-center align-items-md-end">
-        <div class="mb-0 font-weight-light h6">Contact the developer.</div>
-        <div class>
+        <div class="text-center text-md-left  mb-0 font-weight-light text-dark h6"><small>Developed and maintained by <a href="https://www.linkedin.com/in/bhupinder-singh-maan-32aaa8159/" target="_blank" rel="noreferrer" class="text-primary font-weight-bold">Bhupinder Singh</a></small></div>
+        <div class="text-center text-md-left "> 
           <a v-b-tooltip.hover title="Github" class="footer-icons" href="https://github.com/frozen4code" target="_blank" rel="noreferrer" ><i class="fab fa-github-square fa-2x" style="color: #24292E;" aria-label="Github" ></i></a>
           <a v-b-tooltip.hover title="LinkedIn" class="footer-icons" href="https://www.linkedin.com/in/bhupinder-singh-maan-32aaa8159/" target="_blank" rel="noreferrer" ><i class="fab fa-linkedin fa-2x" style="color: #0077B5;" aria-label="LinkedIn" ></i></a>
-          <a v-b-tooltip.hover title="Gmail" class="footer-icons" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=bhpndrsingh306@gmail.com&su=Feedback%20Mobility Eng Portal&tf=1" target="_blank" rel="noreferrer" aria-label="Gmail" ><i class="fas fa-envelope-square fa-2x text-danger"></i></a>
-          <a v-b-tooltip.hover title="Facebook" class="footer-icons" href="https://www.fb.com/bsm990" target="_blank" rel="noreferrer" ><i class="fab fa-facebook-square fa-2x" style="color: #3B5999" aria-label="Facebook"></i></a>
         </div>
       </div>
     </div>
@@ -20,19 +23,11 @@
 </div>
 </template>
 
-<script>
-export default {
-  computed: {},
-  data() {
-    return {};
-  }
-};
-</script>
-
 <style lang="scss">
 .footer {
-  padding-bottom: 1.5rem !important;
+  padding-bottom: 0.5rem !important;
 }
+
 .footer-icons {
   margin-left: 0.2rem;
 }
