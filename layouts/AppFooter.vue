fec4c0c11b99dde516f6d6f2dfdf86e5a1a21db9
frozen4code <bhpndrsingh306@gmail.com>
2018-12-04 18:27:38 +0530

    Re: Footer

--- layouts/AppFooter.vue
+++ layouts/AppFooter.vue
@@ -1,9 +1,6 @@
 <template>
 <div class="container">
-  <div class="row-justify-content-center mb-1">
-    <div class="text-center text-disabled font-weight-light"><small>Copyright © 2018 <a href="http://mobilityeng.in" target="_blank"  rel="noreferrer">Mobility Engineering Consortium Pvt. Ltd.</a> All Rights Reserved.</small></div>
-  </div>
-  <div class="row row-grid align-items-center mx-1">
+  <div class="row row-grid align-items-center mx-1 pt-2">
     <div class="col-md-6 px-0">
       <div class="text-center text-md-left mb-2">
         <a href="mailto:business@mobilityeng.in?subject=MEC Portal | Feedback" target="_blank" rel="noreferrer" class="text-primary font-weight-bold h5">Leave us some feedback!</a>
@@ -13,12 +10,15 @@
     <div class="col-md-6 px-0 mt-0">
       <div class="d-flex flex-column align-items-sm-center align-items-md-end">
         <div class="text-center text-md-left  mb-0 font-weight-light text-dark h6"><small>Developed and maintained by <a href="https://www.linkedin.com/in/bhupinder-singh-maan-32aaa8159/" target="_blank" rel="noreferrer" class="text-primary font-weight-bold">Bhupinder Singh</a></small></div>
-        <div class="text-center text-md-left "> 
+        <div class="text-center text-md-left ">
           <a v-b-tooltip.hover title="Github" class="footer-icons" href="https://github.com/frozen4code" target="_blank" rel="noreferrer" ><i class="fab fa-github-square fa-2x" style="color: #24292E;" aria-label="Github" ></i></a>
           <a v-b-tooltip.hover title="LinkedIn" class="footer-icons" href="https://www.linkedin.com/in/bhupinder-singh-maan-32aaa8159/" target="_blank" rel="noreferrer" ><i class="fab fa-linkedin fa-2x" style="color: #0077B5;" aria-label="LinkedIn" ></i></a>
         </div>
       </div>
     </div>
+  </div>
+  <div class="row justify-content-center mt-1">
+    <div class="text-center text-disabled font-weight-light"><small>Copyright © 2018 <a href="http://mobilityeng.in" target="_blank"  rel="noreferrer">Mobility Engineering Consortium Pvt. Ltd.</a> All Rights Reserved.</small></div>
   </div>
 </div>
 </template>
