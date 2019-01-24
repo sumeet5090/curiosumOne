73ca9057a969d9b38e7a409ee0edb4d888b88c4d
frozen4code <bhpndrsingh306@gmail.com>
2019-01-24 07:39:31 +0530

    Temp notification

--- layouts/custom/Notifications.vue
+++ layouts/custom/Notifications.vue
@@ -1,39 +1,41 @@
 <template>
-    <modal :show.sync="modalState.notifications"
-        gradient="success"
-        style="height: 100vh"
-        modal-classes="modal-danger modal-dialog-centered">
-        <h6 slot="header" class="modal-title display-4" id="modal-title-notification">Notifications</h6>
-        <div class="py-0 text-center">
-            <b-list-group class="modal-custom">
-                <div class="notification" v-for="n in notif" :key="n.id">
-                    <b-list-group-item class="flex-column align-items-start" style="color: black;">
-                        <div class="d-flex w-100 justify-content-between">
-                            <b-link :href="'notification/'+n._id" class="font-weight-bold notification-header">{{n.title}}</b-link>
-                            <b-badge variant="light">{{n.date}}</b-badge>
-                        </div>
-                        <p class="mb-1 font-weight-light notification-text">{{n.description}}</p>
-                    </b-list-group-item>
-                </div>
-            </b-list-group>
-        </div>
-        <template slot="footer">
-            <base-button type="white" @click="modalShowNotification">Ok, Got it</base-button>
-            <base-button type="link"
-                        text-color="white"
-                        class="ml-auto"
-                        @click="modalShowNotification">
-                Close
-            </base-button>
-        </template>
-    </modal>
+<modal :show.sync="modalState.notifications" gradient="success" style="height: 100vh" modal-classes="modal-danger modal-dialog-centered">
+  <h6 slot="header" class="modal-title display-4" id="modal-title-notification">Notifications</h6>
+  <div class="py-0 text-center">
+    <b-list-group class="modal-custom">
+      <div class="notification" v-for="n in notif" :key="n.id">
+        <b-list-group-item class="flex-column align-items-start" style="color: black;">
+          <div class="d-flex w-100 justify-content-between">
+            <b-link :to="n.link" class="font-weight-bold notification-header" @click="modalShowNotification">{{n.title}}</b-link>
+            <b-badge variant="light">{{n.date}}</b-badge>
+          </div>
+          <p class="mb-1 font-weight-light notification-text">
+            <vue-markdown>{{n.description}}</vue-markdown>
+          </p>
+        </b-list-group-item>
+      </div>
+    </b-list-group>
+  </div>
+  <template slot="footer">
+    <base-button type="white" @click="modalShowNotification">Ok, Got it</base-button>
+    <base-button type="link" text-color="white" class="ml-auto" @click="modalShowNotification">
+      Close
+    </base-button>
+  </template>
+</modal>
 </template>
 
 <script>
 import axios from "axios";
-import { mapGetters, mapActions, mapState, mapMutations } from "vuex";
+import {
+  mapGetters,
+  mapActions,
+  mapState,
+  mapMutations
+} from "vuex";
 import Modal from "@/components/Modal";
-import notifService from "@/services/Notification.service";
+import VueMarkdown from "vue-markdown";
+let moment = require("moment");
 export default {
   name: "notifications",
   data() {
@@ -42,7 +44,8 @@ export default {
     };
   },
   components: {
-    Modal
+    Modal,
+    "vue-markdown": VueMarkdown
   },
   computed: {
     ...mapState({
@@ -51,6 +54,7 @@ export default {
   },
   methods: {
     ...mapMutations(["modalShowNotification"]),
+    ...mapActions(["getReq"]),
     formatNotifications(notifications) {
       let temp = "",
         splitDesc = [],
@@ -84,26 +88,42 @@ export default {
     },
     async getNotifications() {
       try {
-        let response = await notifService.getMany();
-        if (!response) {
+        let data = await this.getReq({
+          url: `/api/event/fb2019/announcements`
+        })
+        if (!data) {
           return;
         }
-        this.notif = this.formatNotifications(response.data.notifications);
+        if (process.browser) {
+          if(data.announcements) {
+            let previousCount = localStorage.getItem("NotifCount")
+            let currentCount = data.announcements.length
+            localStorage.setItem("NotifCount", currentCount)
+            // Temporary, will be done later.
+            if (previousCount < currentCount) {
+              this.notif.push({
+                id: 1,
+                title: "New announcements",
+                link: "/event/fb2019/announcements",
+                description: "There may be new notifications for FB2019",
+                date: moment(Date.now()).fromNow()
+              })
+              this.notif = this.formatNotifications(this.notif)
+          }
+          }
+        }
+        // this.notif = data.announcements
       } catch (error) {
         console.log(error);
         return;
       }
     },
-    async clickAndGetNewValues() {
-      let res = await notifService.getMany(); // TODO: Sort latest to oldest
-      if (!res) {
-        return;
-      }
-    }
   },
   mounted() {
-    this.$nextTick(function(){
-      // this.getNotifications(); 
+    this.$nextTick(() => {
+      setInterval( async () => {
+        this.getNotifications();
+      }, 10000)
     })
   }
 };
