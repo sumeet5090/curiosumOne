b0b9db8a2c951569c36ba023b294a2b35141b5ee
frozen4code <bhpndrsingh306@gmail.com>
2019-02-05 20:38:11 +0530

    MEC Downscale

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       host : '159.89.174.161',
       ref  : 'origin/master',
       repo : 'git@github.com:frozen4code/mec_website.git',
-      path : '/home/frozen/prod/mec_website',
+      path : '~/prod/mec_website',
       "forward-agent": true,
       'post-deploy' : 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production --update-env'
     }
